6cdf7c3d61a4c98f15c62ff4802cef5df7e97804
KarthikRIyer <Karthik.iyer2@yandex.com>
2019-01-13 20:22:45 +0530

    Add Change Password Fragment

---
+++ app/src/main/java/com/mdgiitr/karthik/cognizance19/view/ChangePasswordFragment.java
@@ -0,0 +1,30 @@
+package com.mdgiitr.karthik.cognizance19.view;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mdgiitr.karthik.cognizance19.R;
+
+public class ChangePasswordFragment extends Fragment {
+
+    public ChangePasswordFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_change_password, container, false);
+    }
+
+}
