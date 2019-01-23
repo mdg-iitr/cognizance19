c84fb2003908fcfe3e0db4cd271b803a5daaef50
jsuyash1514 <jsuyash1514@gmail.com>
2019-01-23 17:15:29 +0530

    Add tabbed layout in finfest and litfest event frgment

---
+++ app/src/main/java/com/mdgiitr/karthik/cognizance19/view/EventFinfestFragment.java
@@ -0,0 +1,19 @@
+package com.mdgiitr.karthik.cognizance19.view;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mdgiitr.karthik.cognizance19.R;
+
+public class EventFinfestFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event_finfest, container, false);
+    }
+}
