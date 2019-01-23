0276384e48ea70fa9b1a049d878a16ab3e3da017
RITWIK JHA <ritwik0706@gmail.com>
2019-01-24 00:45:04 +0530

    Payment Page

---
+++ app/src/main/java/com/mdgiitr/karthik/cognizance19/view/PaymentFragment.java
@@ -0,0 +1,56 @@
+package com.mdgiitr.karthik.cognizance19.view;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.mdgiitr.karthik.cognizance19.R;
+
+import java.text.DateFormatSymbols;
+
+public class PaymentFragment extends Fragment {
+
+    Spinner monthSpinner, yearSpinner;
+
+    public PaymentFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_payment, container, false);
+
+        monthSpinner = v.findViewById(R.id.monthSpinner);
+        yearSpinner = v.findViewById(R.id.yearSpinner);
+
+        DateFormatSymbols symbols = new DateFormatSymbols();
+        String[] monthNames = symbols.getMonths();
+
+        int[] years;
+        years = new int[50];
+
+        for (int i=0; i<50; i++){
+            years[i] = 2019+i;
+        }
+
+        ArrayAdapter<String> monthAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, monthNames);
+        monthAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        monthSpinner.setAdapter(monthAdapter);
+
+        ArrayAdapter <Integer> yearAdapter = new ArrayAdapter <Integer>(this.getActivity(), android.R.layout.simple_spinner_item, years);
+        yearAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        monthSpinner.setAdapter(yearAdapter);
+
+        return v;
+    }
+}
