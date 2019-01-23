8e5f35e6094da62125f03f4826aee4581cebfda2
nikhilagrawal2000 <nikhilagrawal1616@gmail.com>
2019-01-23 19:13:50 +0530

    Add Spotlight Fragment

---
+++ app/src/main/java/com/mdgiitr/karthik/cognizance19/adapters/SpotlightsTabViewPagerAdapter.java
@@ -0,0 +1,21 @@
+package com.mdgiitr.karthik.cognizance19.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SpotlightsTabViewPagerAdapter extends FragmentPagerAdapter {
+    public SpotlightsTabViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
