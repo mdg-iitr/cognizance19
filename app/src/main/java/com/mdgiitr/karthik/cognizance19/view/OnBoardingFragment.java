0e3094b49136e60e48240e048d4a9b5fcfb68545
Karthik Ramesh Iyer <Karthik.iyer2@yandex.com>
2019-01-23 19:52:00 +0530

    Implement state and city autocomplete text view

--- app/src/main/java/com/mdgiitr/karthik/cognizance19/view/OnBoardingFragment.java
+++ app/src/main/java/com/mdgiitr/karthik/cognizance19/view/OnBoardingFragment.java
@@ -2,6 +2,7 @@ package com.mdgiitr.karthik.cognizance19.view;
 
 
 import android.os.Bundle;
+import android.support.design.card.MaterialCardView;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +16,8 @@ import static com.mdgiitr.karthik.cognizance19.MainActivity.navController;
 
 public class OnBoardingFragment extends Fragment {
 
-    CircleImageView smallImageView;
+    private CircleImageView smallImageView;
+    private MaterialCardView completeProfileCardView;
 
     public OnBoardingFragment() {
         // Required empty public constructor
@@ -31,10 +33,16 @@ public class OnBoardingFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_onboarding, container, false);
 
+        completeProfileCardView = view.findViewById(R.id.complete_you_profile_cardView);
+
+        completeProfileCardView.setOnClickListener((View v) -> {
+            navController.navigate(R.id.action_onBoardingFragment_to_completeYourProfileFragment);
+        });
+
         smallImageView = view.findViewById(R.id.small_profile_image);
 
         smallImageView.setOnClickListener((View v) -> {
-            navController.navigate(R.id.action_onBoardingFragment_to_myProfileFragment);
+//            navController.navigate(R.id.action_onBoardingFragment_to_myProfileFragment);
         });
 
         return view;
